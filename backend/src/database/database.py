ee834d64c095a6b704ba778d42e9df9bb65e5059
Raissa Kelly Brito de Araújo <20231101110069@ifrn.local>
2026-06-23 15:29:19 -0300

    Pág de homepage

--- backend/src/database/database.py
+++ backend/src/database/database.py
@@ -11,7 +11,7 @@ DB = URL.create(
     username=getenv("DB_USERNAME"),
     password=getenv("DB_PASSWORD"),
     host=getenv("DB_HOST"),
-    port=getenv("DB_PORT"),
+    port=getenv("DB_PORT"),\
     database=getenv("DB_NAME")
 )
 
