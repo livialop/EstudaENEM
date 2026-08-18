e35a0cfd20880b851ae003354dc365e5e10ec5a2
livialop <livvialn7@gmail.com>
2026-08-18 08:57:17 -0300

    add alembic ao projeto

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,5 +2,5 @@ import uvicorn
 from database.database import create_db, DB
 
 if __name__ == "__main__":
-    create_db(DB)
+    # create_db(DB)
     uvicorn.run("app.api:app", host="0.0.0.0", port=8080)
