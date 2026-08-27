29b68e619f4ab01500f005e3487cba788aa8783f
livialop <livvialn7@gmail.com>
2026-08-27 17:53:16 -0300

    conexao login e cadastro com o backend

--- frontend/estuda-enem-app/src/pages/login/LoginForm.jsx
+++ frontend/estuda-enem-app/src/pages/login/LoginForm.jsx
@@ -1,6 +1,61 @@
 import {Mail, Lock } from "lucide-react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginForm() {
+
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const [mensagem, setMensagem] = useState("");
+  const [erro, setErro] = useState("");
+
+  const navigate = useNavigate()
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    setMensagem("");
+    setErro("");
+
+    try {
+
+      const dados = new URLSearchParams();
+      dados.append("username", email)
+      dados.append("password", senha)
+      
+      const response = await fetch("http://localhost:8080/auth/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+          credentials: "include",
+          body: dados // nao dava p/ usar o json.stringify pq o auth request form do jwt requer outro formato de dados
+          });
+
+        const data = await response.json();
+
+        if (!response.ok) {
+          setErro(data.detail || "Erro ao fazer login.");
+          return;
+        }
+
+        setMensagem("Login realizado com sucesso!");
+
+        // campos limpos
+        setEmail("");
+        setSenha("");
+
+        navigate("/"); // TODO: ALTERAR ISSO DEPOIS PARA ÁREA INICIAL (PAG N EXISTE AINDA)
+
+    } catch (error) {
+      setErro("Erro ao fazer login.");
+      console.error(error);
+    }
+
+  }
+
   return (
     <section className="login-card">
 
@@ -14,23 +69,49 @@ export default function LoginForm() {
 
       <hr />
 
-      <form>
+      <form onSubmit={handleSubmit}>
 
         <label>E-mail</label>
 
         <div className="input">
           <Mail size={16}/>
-          <input placeholder="Digite seu e-mail"/>
+          
+          <input 
+            type="email"
+            placeholder="Digite seu e-mail"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            required
+          />
+          
         </div>
 
         <label>Senha</label>
 
         <div className="input">
           <Lock size={16}/>
-          <input type="password" placeholder="Crie sua senha"/>
+          
+          <input 
+            type="password" 
+            placeholder="Digite sua senha"
+            value={senha}
+            onChange={(event) => setSenha(event.target.value)}
+            required
+          />
+          
         </div>
 
-        <button className="login-button">
+        {/* o && funciona assim: se a expressao da esquerda for true ele exibe a da direita
+        logo, se tiver erro, exibe a msg de erro
+        se tiver sucesso, exibe a msg */}
+        {erro && <p className="error">{erro}</p>} 
+
+        {mensagem && <p className="success">{mensagem}</p>}
+
+        <button 
+          type="submit"
+          className="login-button"
+        >
           Fazer login
         </button>
 
--- frontend/estuda-enem-app/src/pages/register/RegisterForm.jsx
+++ frontend/estuda-enem-app/src/pages/register/RegisterForm.jsx
@@ -1,9 +1,71 @@
+import { useState } from "react";
 import { User, Mail, Lock } from "lucide-react";
-
-// TODO testar com o backend
+import { useNavigate } from "react-router-dom";
 
 export default function RegisterForm() {
+
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [confirmarSenha, setConfirmarSenha] = useState("");
+  
+  const [mensagem, setMensagem] = useState("");
+  const [erro, setErro] = useState("");
+  
+  const navigate = useNavigate();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    setMensagem("");
+    setErro("");
+
+
+    if (senha !== confirmarSenha) {
+      setErro("As senhas estão diferentes.");
+      return;
+    }
+
+    try {
+      const response = await fetch("http://localhost:8080/auth/cadastro", 
+        {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          nome: nome,
+          email: email,
+          senha: senha,
+        }),
+      });
+
+      const data = await response.json();
+
+      // checa com o backend se deu erro na requisição
+      if (!response.ok) {
+        setErro(data.detail || "Erro ao cadastrar usuário.");
+        return;
+      }
+
+      setMensagem("Cadastro realizado com sucesso!");
+
+      // limpa os campos
+      setNome("");
+      setEmail("");
+      setSenha("");
+      setConfirmarSenha("");
+
+      navigate("/login"); //redireciona pro login após realizar cadastro.
+
+    } catch (error) {
+      setErro("Não foi possível conectar ao servidor.");
+      console.error(error);
+    }
+  }
+
   return (
+    // manti o form igual, so alterei p se conectar com o backend @raissa
     <section className="register-card">
 
       <h1>Crie sua conta</h1>
@@ -15,37 +77,77 @@ export default function RegisterForm() {
 
       <hr />
 
-      <form>
+      <form onSubmit={handleSubmit}>
 
         <label>Nome completo</label>
 
         <div className="input">
-          <User size={16}/>
-          <input placeholder="Digite seu nome completo"/>
+          <User size={16} />
+
+          <input
+            type="text"
+            placeholder="Digite seu nome completo"
+            value={nome}
+            onChange={(event) => setNome(event.target.value)}
+            required
+          />
         </div>
+
 
         <label>E-mail</label>
 
         <div className="input">
-          <Mail size={16}/>
-          <input placeholder="Digite seu e-mail"/>
+          <Mail size={16} />
+
+          <input
+            type="email"
+            placeholder="Digite seu e-mail"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            required
+          />
         </div>
+
 
         <label>Senha</label>
 
         <div className="input">
-          <Lock size={16}/>
-          <input type="password" placeholder="Crie sua senha"/>
+          <Lock size={16} />
+
+          <input
+            type="password"
+            placeholder="Crie sua senha"
+            value={senha}
+            onChange={(event) => setSenha(event.target.value)}
+            required
+          />
         </div>
+
 
         <label>Confirmar senha</label>
 
         <div className="input">
-          <Lock size={16}/>
-          <input type="password" placeholder="Confirme sua senha"/>
+          <Lock size={16} />
+
+          <input
+            type="password"
+            placeholder="Confirme sua senha"
+            value={confirmarSenha}
+            onChange={(event) => setConfirmarSenha(event.target.value)}
+            required
+          />
         </div>
 
-        <button className="register-button">
+        {/* TODO: COLOCAR A COR DA MENSAGEM DE ERRO PARA VERMELHO */}
+        {erro && <p className="error">{erro}</p>} 
+
+        {mensagem && <p className="success">{mensagem}</p>}
+
+
+        <button
+          type="submit"
+          className="register-button" 
+        >
           Cadastrar-se
         </button>
 
