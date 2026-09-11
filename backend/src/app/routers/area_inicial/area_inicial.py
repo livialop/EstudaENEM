56f58eb8d9a3e6441070eb2564e136ae04991a36
raiwyzs <raissak776@gmail.com>
2026-09-11 07:55:23 -0300

    Merge branch 'main' of https://github.com/livialop/EstudaENEM

---
+++ backend/src/app/routers/area_inicial/area_inicial.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter, Depends
+from models.model import Usuario
+from app.routers.auth.auth import get_usuario_logado
+
+area_ini_router = APIRouter(prefix="", tags=["Área Inicial"])
+
+@area_ini_router.get("/")
+def area_inicial(
+    current_user: Usuario = Depends(get_usuario_logado)
+):
+    return {
+        "message": "Acessou a área inicial",
+        "usuario": current_user
+    }
